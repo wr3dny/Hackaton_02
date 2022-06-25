46a6e42e58bde516fad98da70b8529d5570cc58d
wr3dny <you@example.com>
2022-06-25 16:40:59 +0200

    problem

--- RPG_port.py
+++ RPG_port.py
@@ -1,13 +1,19 @@
+import random
+
+quest_p1 = ['You can make a few coins unloading ship cargo', 'Help rebuild inn', 'Work as bodyguard in tavern']
+
 def port():
-    import RPG_enemy as enemy
-    print(f'On your way you come upon {enemy}')
-    print('Fight or run (r/f)')
-    choice = input()
-    if choice.capitalize() == 'R':
-        run = 'run'
-        return run
-    else:
-        fight = 'fight'
-        return fight
+    print(random.choice(quest_p1))
+    choice = input('y/n')
+    i = 0
+    for i <== 0>:
+        if choice.capitalize() == 'y':
+            coin = random.randint(1, 5)
+        elif choice.capitalize() == 'n':
+            print(random.choice(quest_p1))
+
+
+
+
 
 port()
---
+++ problem.py
@@ -0,0 +1,4 @@
+import RPG_enemy as enemy
+
+print(f'On your way you come upon {enemy}')
+
