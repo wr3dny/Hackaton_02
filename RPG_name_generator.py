660d13d12e2f7f6e050ffba58349fc06e7a83027
wr3dny <you@example.com>
2022-06-25 10:23:41 +0200

    (re)creating basic

---
+++ RPG_name_generator.py
@@ -0,0 +1,32 @@
+# Generates name
+
+import random
+
+vowel = ['a', 'e', 'i', 'o', 'u']
+consonant = ['b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'n', 'q', 's', 't', 'v',
+             'x', 'z', 'h', 'r', 'w', 'y']
+name_parts = []
+i = 0
+while i <= 200:
+    vr = random.choice(vowel)
+    cr = random.choice(consonant)
+    syllables = cr + vr
+    name_parts.append(syllables)
+    i += 1
+    name_parts = list(set(name_parts))
+
+
+def name_generator():
+    generated_name = []
+    name_lenght = random.randint(1, 4)
+    j = 0
+    for j in range(0, name_lenght):
+        generated_name.append(random.choice(name_parts))
+        j += 1
+
+    name_out: str = ' '.join(generated_name)
+    name_out = name_out.replace(' ', '')
+    name_out = name_out.capitalize()
+    return name_out
+
+name_generator()
--- RatherPoorGame.py
+++ RatherPoorGame.py
@@ -1,1 +1,30 @@
 # RPG
+# step by step so called 'concept'
+
+
+def name_generator():
+    pass
+
+
+def hero_sex():
+    pass
+
+
+def hero_attributes():
+    pass
+
+
+def inventory():
+    pass
+
+
+def crossroads():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__mian__':
+    main()
